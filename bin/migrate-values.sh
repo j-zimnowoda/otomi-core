23a951ca2a4b331515f13844ba429508950f56ea
svatwork <sebastiaan.verbeek@redkubes.com>
2021-01-18 14:53:39 +0100

    feat: prepare env for demo

--- bin/migrate-values.sh
+++ bin/migrate-values.sh
@@ -2,4 +2,12 @@
 
 set -e
 
-docker run --rm otomi/tasks:migrate-values npm run tasks:migrate-values
+docker run \
+  -v ${ENV_DIR}/env:/app/dist/tasks/migrate-values/env \
+  -v ${PWD}/bin/mock-changes.yaml:/app/dist/tasks/migrate-values/mock-changes.yaml \
+  --rm \
+  otomi/tasks:migrate-values npm run tasks:migrate-values -- \
+  --env-dir /app/dist/tasks/migrate-values/env \
+  --changes /app/dist/tasks/migrate-values/mock-changes.yaml \
+  --file mock.yaml \
+  --op displacements
