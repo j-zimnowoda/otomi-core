bc717ba07fa4286e756d838b86bee608ee3801b1
svatwork <sebastiaan.verbeek@redkubes.com>
2021-01-19 08:27:01 +0100

    fix: clean script up a bit for demo

--- bin/migrate-values.sh
+++ bin/migrate-values.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+migrate_values_dir=/app/dist/tasks/migrate-values
+
 docker run \
-  -v ${ENV_DIR}/env:/app/dist/tasks/migrate-values/env \
-  -v ${PWD}/bin/mock-changes.yaml:/app/dist/tasks/migrate-values/mock-changes.yaml \
+  -v ${ENV_DIR}/env:${migrate_values_dir}/env \
+  -v ${PWD}/bin/mock-changes.yaml:${migrate_values_dir}/mock-changes.yaml \
   --rm \
   otomi/tasks:migrate-values npm run tasks:migrate-values -- \
-  --env-dir /app/dist/tasks/migrate-values/env \
-  --changes /app/dist/tasks/migrate-values/mock-changes.yaml \
+  --env-dir ${migrate_values_dir}/env \
+  --changes ${migrate_values_dir}/mock-changes.yaml \
   --file mock.yaml \
   --op displacements
