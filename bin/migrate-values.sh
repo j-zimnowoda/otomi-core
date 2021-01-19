722090ed3525f55f69b8e88607a7a782a5eba79e
svatwork <sebastiaan.verbeek@redkubes.com>
2021-01-19 09:45:25 +0100

    feat: demo iteration

--- bin/migrate-values.sh
+++ bin/migrate-values.sh
@@ -4,12 +4,18 @@ set -e
 
 migrate_values_dir=/app/dist/tasks/migrate-values
 
+# PARAMETERS
+env_dir=${migrate_values_dir}/env
+changes=${migrate_values_dir}/mock-changes.yaml
+file=mock.yaml
+op=displacements
+
 docker run \
-  -v ${ENV_DIR}/env:${migrate_values_dir}/env \
+  -v ${ENV_DIR}/env:${env_dir} \
   -v ${PWD}/bin/mock-changes.yaml:${migrate_values_dir}/mock-changes.yaml \
   --rm \
   otomi/tasks:migrate-values npm run tasks:migrate-values -- \
-  --env-dir ${migrate_values_dir}/env \
-  --changes ${migrate_values_dir}/mock-changes.yaml \
-  --file mock.yaml \
-  --op displacements
+  --env-dir ${env_dir} \
+  --changes ${changes} \
+  --file ${file} \
+  --op ${op}
