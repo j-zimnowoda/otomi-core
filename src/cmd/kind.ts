c136aa376966e29080a77d9115568094f9266d62
svatwork <ajsverbeek@gmail.com>
2021-07-01 15:49:12 +0200

    feat: initial kind script

--- src/cmd/kind.ts
+++ src/cmd/kind.ts
@@ -3,22 +3,9 @@ import { OtomiDebugger, terminal } from '../common/debug'
 import { BasicArguments, ENV } from '../common/no-deps'
 import { cleanupHandler, otomi, PrepareEnvironmentOptions } from '../common/setup'
 
-/* Steps:
- * 1. Follow all TODO in this file
- * 2. Update src/cmd/index.ts and add:
- *      `import <fileName>Module from './<fileName>'`
- *      `export { default as <fileName> } from './<fileName>'
- *      add `<fileName>Module` to commands constant
- */
+type Arguments = BasicArguments
 
-// TODO: extend this interface with the HelmArguments from '../helm.opts.ts' or add the options that you define in the `builder` at the bottom
-interface Arguments extends BasicArguments {
-  // TODO: Define custom options, if necessary
-  TODO?: string
-}
-
-// TODO: Rename fileName var to name of file / otomi command
-const fileName = 'example'
+const fileName = 'kind'
 let debug: OtomiDebugger
 
 /* eslint-disable no-useless-return */
@@ -34,23 +21,21 @@ const setup = async (argv: Arguments, options?: PrepareEnvironmentOptions): Prom
   if (options) await otomi.prepareEnvironment(debug, options)
 }
 
-// TODO: Rename function name to filename
-export const example = async (argv: Arguments, options?: PrepareEnvironmentOptions): Promise<void> => {
+export const kind = async (argv: Arguments, options?: PrepareEnvironmentOptions): Promise<void> => {
   await setup(argv, options)
 
-  // TODO: Write your code here
-  console.log(fileName)
-  console.log(argv)
+  debug.log(fileName)
+  debug.log(argv)
 }
 
 export const module = {
   command: fileName,
-  describe: '',
+  describe: 'kind',
   builder: (parser: Argv): Argv => parser,
 
   handler: async (argv: Arguments): Promise<void> => {
     ENV.PARSED_ARGS = argv
-    await example(argv, {}) // TODO: Replace with function name
+    await kind(argv, {})
   },
 }
 
