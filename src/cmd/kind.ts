439301148cc0bafab1228964a79afbe53ebdef0a
svatwork <ajsverbeek@gmail.com>
2021-07-06 12:40:16 +0200

    feat: create delete poc

--- src/cmd/kind.ts
+++ src/cmd/kind.ts
@@ -22,10 +22,25 @@ const setup = async (argv: Arguments, options?: PrepareEnvironmentOptions): Prom
   if (options) await otomi.prepareEnvironment(debug, options)
 }
 
+function guidGenerator() {
+  const S4 = function () {
+    // eslint-disable-next-line no-bitwise
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1)
+  }
+  return `${S4() + S4()}-${S4()}-${S4()}-${S4()}-${S4()}${S4()}${S4()}`
+}
+
 export const kind = async (argv: Arguments, options?: PrepareEnvironmentOptions): Promise<void> => {
   await setup(argv, options)
 
-  console.log(await $`/usr/local/bin/kind -h`)
+  const buildID = guidGenerator()
+  const tags = ['v1.19.0']
+  const KUBECONFIG = `$HOME/.kube/configs/${buildID}`
+
+  await $`curl -sSL https://get.docker.com/ | sh`
+  await $`export KUBECONFIG=${KUBECONFIG}`
+  await $`kind create cluster --wait 30s --name=${buildID} --image=kindest/node:${tags[0]}`
+  await $`kind delete cluster --name=${buildID}`
 }
 
 export const module = {
