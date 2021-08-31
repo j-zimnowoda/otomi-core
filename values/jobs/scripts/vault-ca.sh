2ae0d25f7c5c79d8b84930940a585db8ad31ea26
svatwork <ajsverbeek@gmail.com>
2021-08-31 09:47:24 +0200

    feat: vault pki script

--- values/jobs/scripts/vault-ca.sh
+++ values/jobs/scripts/vault-ca.sh
@@ -1,3 +1,25 @@
 set -e
 
 vault secrets enable pki
+vault secrets tune -max-lease-ttl=87600h pki
+vault write -field=certificate pki/root/generate/internal \
+     common_name="otomi.cloud" \
+     ttl=87600h > CA_cert.crt
+vault write pki/config/urls \
+     issuing_certificates="$VAULT_ADDR/v1/pki/ca" \
+     crl_distribution_points="$VAULT_ADDR/v1/pki/crl"
+
+vault secrets enable -path=pki_int pki
+vault secrets tune -max-lease-ttl=43800h pki_int
+vault write -format=json pki_int/intermediate/generate/internal \
+     common_name="otomi.cloud Intermediate Authority" \
+     | jq -r '.data.csr' > pki_intermediate.csr
+vault write -format=json pki/root/sign-intermediate csr=@pki_intermediate.csr \
+     format=pem_bundle ttl="43800h" \
+     | jq -r '.data.certificate' > intermediate.cert.pem
+vault write pki_int/intermediate/set-signed certificate=@intermediate.cert.pem
+
+vault write pki_int/roles/otomi-cloud \
+     allowed_domains="otomi.cloud" \
+     allow_subdomains=true \
+     max_ttl="720h"
